a00e4850ce3766646610fe7bd5c2db9a317820a2
Ronald Bradford <me@ronaldbradford.com>
2015-10-07 13:39:50 -0400

    Added initial navigation bar for intro pages

---
+++ fonts/sync.sh
@@ -0,0 +1,6 @@
+wget -O glyphicons-halflings-regular.eot http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/fonts/glyphicons-halflings-regular.eot
+wget -O glyphicons-halflings-regular.svg http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/fonts/glyphicons-halflings-regular.svg
+wget -O glyphicons-halflings-regular.ttf http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/fonts/glyphicons-halflings-regular.ttf
+wget -O glyphicons-halflings-regular.woff http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/fonts/glyphicons-halflings-regular.woff
+wget -O glyphicons-halflings-regular.woff2 http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/fonts/glyphicons-halflings-regular.woff2
+
