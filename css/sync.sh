75c9cecbe856e5583102891d35603951d0761349
Ronald Bradford <me@ronaldbradford.com>
2015-10-07 11:34:36 -0400

    Obtain local version of CSS/JS libraries for offline use

---
+++ css/sync.sh
@@ -0,0 +1,3 @@
+wget -O bootstrap.min.css http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css
+wget -O font-awesome.min.css http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css
+
---
+++ js/sync.sh
@@ -0,0 +1,4 @@
+wget -O jquery.min.js http://code.jquery.com/jquery-2.1.3.min.js
+wget -O bootstrap.min.js  http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js
+wget -O jquery.validate.min.js http://cdn.jsdelivr.net/jquery.validation/1.13.1/jquery.validate.min.js
+
